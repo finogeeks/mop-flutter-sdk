fdba96465c442089c8e1b6f21a60dce50f710dc5
wangtao <wangtao@finogeeks.com>
2021-06-28 10:17:27 +0800

    fix:iOS一码通的返回结果改成model

--- ios/Classes/Api/MOP_parseAppletInfoFromWXQrCode.m
+++ ios/Classes/Api/MOP_parseAppletInfoFromWXQrCode.m
@@ -13,11 +13,15 @@
 
 - (void)setupApiWithSuccess:(void (^)(NSDictionary<NSString *,id> * _Nonnull))success failure:(void (^)(id _Nullable))failure cancel:(void (^)(void))cancel {
     NSLog(@"MOP_parseWXQrCode：%@", self.qrCode);
-    [[FATClient sharedClient] parseAppletInfoFromWXQrCode:self.qrCode apiServer:self.apiServer completion:^(NSDictionary *result, FATError *aError) {
+    [[FATClient sharedClient] parseAppletInfoFromWXQrCode:self.qrCode apiServer:self.apiServer completion:^(FATAppletSimpleInfo *appInfo, FATError *aError) {
         if (aError) {
             failure(aError.description);
         }else{
-            success(result);
+            NSMutableDictionary *dataDic = [[NSMutableDictionary alloc]init];
+            if (appInfo && appInfo.appId) {
+                [dataDic setObject:appInfo.appId forKey:@"appId"];
+            }
+            success(dataDic);
         }
     }];
 }
--- ios/mop.podspec
+++ ios/mop.podspec
@@ -16,7 +16,7 @@ A finclip miniprogram flutter sdk.
   s.public_header_files = 'Classes/**/*.h'
   s.dependency 'Flutter'
   s.ios.deployment_target = '9.0'
-  s.dependency 'FinApplet' , '2.31.0-alpha20210624v05'
-  s.dependency 'FinAppletExt' , '2.31.0-alpha20210624v05'
+  s.dependency 'FinApplet' , '2.31.0-alpha20210628v02'
+  s.dependency 'FinAppletExt' , '2.31.0-alpha20210628v02'
 end
 
