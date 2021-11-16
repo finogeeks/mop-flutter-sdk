9207aa8c833dc85285a7a01d6ff18710f30ba0b1
wzy <wangzhaoyao@finogeeks.com>
2021-11-16 10:51:55 +0800

    1.新增调用高德地图

--- ios/Classes/Api/MOP_initialize.m
+++ ios/Classes/Api/MOP_initialize.m
@@ -51,6 +51,8 @@
         return;
     }
     [[FATExtClient sharedClient] fat_prepareExtensionApis];
+    [[FATExtClient sharedClient] fat_UsingMapType:@"FATExtMapStyleGD" MapKey:@"6f0f28c4138cbaa51aa5890e26996ea2"];
+
     [[FATClient sharedClient] setEnableLog:YES];
     success(@{});
     
