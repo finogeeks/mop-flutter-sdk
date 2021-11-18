e6c95fe8262c244c121efca7bb3378be1356cfe2
wzy <wangzhaoyao@finogeeks.com>
2021-11-18 09:32:17 +0800

    1.注释stable分支会报错的代码

--- ios/Classes/Api/MOP_initialize.m
+++ ios/Classes/Api/MOP_initialize.m
@@ -51,7 +51,7 @@
         return;
     }
     [[FATExtClient sharedClient] fat_prepareExtensionApis];
-    [[FATExtClient sharedClient] fat_UsingMapType:@"FATExtMapStyleGD" MapKey:@"6f0f28c4138cbaa51aa5890e26996ea2"];
+//    [[FATExtClient sharedClient] fat_UsingMapType:@"FATExtMapStyleGD" MapKey:@"6f0f28c4138cbaa51aa5890e26996ea2"];
 
     [[FATClient sharedClient] setEnableLog:YES];
     success(@{});
