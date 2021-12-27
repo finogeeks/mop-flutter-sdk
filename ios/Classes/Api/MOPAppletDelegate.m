04665b39346d319d8b0bcc25eeff57a136a3a1dc
hujianhui <hujianhui@finogeeks.com>
2021-12-27 18:22:04 +0800

    添加到桌面

--- ios/Classes/Api/MOPAppletDelegate.m
+++ ios/Classes/Api/MOPAppletDelegate.m
@@ -76,6 +76,9 @@
             FATAppletMenuStyle style = [typeString isEqualToString:@"onMiniProgram"] ? FATAppletMenuStyleOnMiniProgram : FATAppletMenuStyleCommon;
             model.menuType = style;
         }
+        if ([@"Desktop" isEqualToString:model.menuId] && FATAppletVersionTypeRelease != appletInfo.appletVersionType) {
+            continue;
+        }
         [models addObject:model];
     }
     
