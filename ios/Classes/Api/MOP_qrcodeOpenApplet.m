8837bd5614a103983eff6d7fe8d25149e97fdb69
wanghualei <wanghualei@finogeeks.com>
2021-12-29 10:11:50 +0800

    add 二维码打开小程序

---
+++ ios/Classes/Api/MOP_qrcodeOpenApplet.m
@@ -0,0 +1,37 @@
+//
+//  MOP_scanOpenApplet.m
+//  mop
+//
+//  Created by beetle_92 on 2021/6/7.
+//
+
+#import "MOP_qrcodeOpenApplet.h"
+#import "MOPTools.h"
+#import <FinApplet/FinApplet.h>
+
+@implementation MOP_qrcodeOpenApplet
+
+- (void)setupApiWithSuccess:(void (^)(NSDictionary<NSString *,id> * _Nonnull))success failure:(void (^)(id _Nullable))failure cancel:(void (^)(void))cancel {
+    NSLog(@"MOP_qrcodeOpenApplet:%@", self.qrcode);
+    FATAppletQrCodeRequest *qrcodeRequest = [[FATAppletQrCodeRequest alloc] init];
+    qrcodeRequest.qrCode = self.qrcode;
+
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIViewController *currentVC = [MOPTools topViewController];
+        [[FATClient sharedClient] startAppletWithQrCodeRequest:qrcodeRequest inParentViewController:currentVC requestBlock:^(BOOL result, FATError *error) {
+            NSLog(@"请求完成：%@", error);
+        } completion:^(BOOL result, FATError *error) {
+            NSLog(@"打开完成：%@", error);
+            if (result){
+                success(@{});
+            } else {
+                failure(error.description);
+            }
+        } closeCompletion:^{
+            NSLog(@"关闭");
+        }];
+    });
+    
+}
+
+@end
--- ios/mop.podspec
+++ ios/mop.podspec
@@ -17,7 +17,7 @@ A finclip miniprogram flutter sdk.
   s.dependency 'Flutter'
   s.ios.deployment_target = '9.0'
 
-  s.dependency 'FinApplet' , '2.34.12'
-  s.dependency 'FinAppletExt' , '2.34.12'
+  s.dependency 'FinApplet' , '2.35.0-alpha20211229v02'
+  s.dependency 'FinAppletExt' , '2.35.0-alpha20211229v02'
 end
 
