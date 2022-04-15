4f7004630d28a8b63ddef7ba57a71b1859f9bd19
zhongweiguang <zhongweiguang@finogeeks.com>
2022-04-15 15:25:26 +0800

    修复编译报错

--- android/src/main/java/com/finogeeks/mop/api/mop/AppletHandlerModule.java
+++ android/src/main/java/com/finogeeks/mop/api/mop/AppletHandlerModule.java
@@ -259,7 +259,7 @@ public class AppletHandlerModule extends BaseApi {
 
             @Override
             public boolean feedback(@NotNull Bundle bundle) {
-                return false
+                return false;
             }
 
         });
