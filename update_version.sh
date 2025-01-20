b77a0e779db220cf951507f0bd90b3772812d419
wanghualei <wanghualei@finogeeks.com>
2025-01-20 18:48:51 +0800

    update stage update

--- update_version.sh
+++ update_version.sh
@@ -20,19 +20,25 @@ cp -r pubspec.tpl.yaml pubspec.yaml
 sed -i "" "s/__mop_flutter_sdk_version__/${version}/g" pubspec.yaml
 
 # 更新iOS podspec
-if [ -n "$iosVersion" ]
-then
-echo "更新mop.podspec====>"
-cp -r ios/mop.podspec.tpl ios/mop.podspec
-sed -i "" "s/__finapplet_version__/${iosVersion}/g" ios/mop.podspec
-
+if [ -n "$iosVersion" ]; then
+    echo "更新mop.podspec====>"
+    cp -r ios/mop.podspec.tpl ios/mop.podspec
+    sed -i "" "s/__finapplet_version__/${iosVersion}/g" ios/mop.podspec
+else
+    echo "跳过 iOS podspec 更新（未设置版本号）"
 fi
 
 # 更新android gradle
-if [ -n "$androidVersion" ]
-then
-echo "更新build.gradle====>"
-cp -r android/build.gradle.tpl android/build.gradle
-sed -i "" "s/__finapplet_version__/${androidVersion}/g" android/build.gradle
-
+if [ -n "$androidVersion" ]; then
+    echo "更新build.gradle====>"
+    cp -r android/build.gradle.tpl android/build.gradle
+    sed -i "" "s/__finapplet_version__/${androidVersion}/g" android/build.gradle
+else
+    echo "跳过 Android gradle 更新（未设置版本号）"
 fi
+
+git remote add ssh-origin ssh://git@gitlab.finogeeks.club:2233/finclipsdk/finclip-flutter-sdk.git
+
+git add .
+git commit -m "update version:$version"
+git push ssh-origin 
