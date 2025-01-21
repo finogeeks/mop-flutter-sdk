129c6316ce77f9d02eb504a7dfc12d3cc55a01f9
wanghualei <wanghualei@finogeeks.com>
2025-01-21 10:56:36 +0800

    update

--- update_version.sh
+++ update_version.sh
@@ -3,9 +3,9 @@ export FASTLANE_DISABLE_COLORS=1
 
 
 # 接收参数并去除可能的引号
-version=$(echo "$1" | tr -d '"')
-iosVersion=$(echo "$2" | tr -d '"')
-androidVersion=$(echo "$3" | tr -d '"')
+version=$1
+iosVersion=$2
+androidVersion=$3
 
 #version=`git describe --abbrev=0 --tags | tr -d '\\n' | tr -d '\\t'`
 
@@ -21,7 +21,7 @@ cp -r pubspec.tpl.yaml pubspec.yaml
 sed -i "" "s/__mop_flutter_sdk_version__/${version}/g" pubspec.yaml
 
 # 更新iOS podspec
-if [ -n "$iosVersion" ]; then
+if [ -n "$iosVersion" && "$iosVersion" != "none" ]; then
     echo "更新mop.podspec====>"
     cp -r ios/mop.podspec.tpl ios/mop.podspec
     sed -i "" "s/__finapplet_version__/${iosVersion}/g" ios/mop.podspec
@@ -30,7 +30,7 @@ else
 fi
 
 # 更新android gradle
-if [ -n "$androidVersion" ]; then
+if [ -n "$androidVersion" && "$androidVersion" != "none" ]; then
     echo "更新build.gradle====>"
     cp -r android/build.gradle.tpl android/build.gradle
     sed -i "" "s/__finapplet_version__/${androidVersion}/g" android/build.gradle
@@ -38,8 +38,8 @@ else
     echo "跳过 Android gradle 更新（未设置版本号）"
 fi
 
-git remote add ssh-origin ssh://git@gitlab.finogeeks.club:2233/finclipsdk/finclip-flutter-sdk.git
+# git remote add ssh-origin ssh://git@gitlab.finogeeks.club:2233/finclipsdk/finclip-flutter-sdk.git
 
-git add .
-git commit -m "update version:$version"
-git push ssh-origin 
+# git add .
+# git commit -m "update version:$version"
+# git push ssh-origin 
