1b30245d6bbb92af5cd59b4f754fc8cd3027757f
wanghualei <wanghualei@finogeeks.com>
2025-01-21 10:59:43 +0800

    update

--- update_version.sh
+++ update_version.sh
@@ -21,7 +21,7 @@ cp -r pubspec.tpl.yaml pubspec.yaml
 sed -i "" "s/__mop_flutter_sdk_version__/${version}/g" pubspec.yaml
 
 # 更新iOS podspec
-if [ -n "$iosVersion" && "$iosVersion" != "none" ]; then
+if [[ -n "$iosVersion" && "$iosVersion" != "none" ]]; then
     echo "更新mop.podspec====>"
     cp -r ios/mop.podspec.tpl ios/mop.podspec
     sed -i "" "s/__finapplet_version__/${iosVersion}/g" ios/mop.podspec
@@ -30,7 +30,7 @@ else
 fi
 
 # 更新android gradle
-if [ -n "$androidVersion" && "$androidVersion" != "none" ]; then
+if [[ -n "$androidVersion" && "$androidVersion" != "none" ]]; then
     echo "更新build.gradle====>"
     cp -r android/build.gradle.tpl android/build.gradle
     sed -i "" "s/__finapplet_version__/${androidVersion}/g" android/build.gradle
