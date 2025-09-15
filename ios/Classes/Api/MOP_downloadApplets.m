24704aeb1eea4479f04b94878f842a530d36034a
chenfanfang <493336001@finogeeks.com>
2025-09-15 18:07:38 +0800

    feat: 耗时函数的处理

--- ios/Classes/Api/MOP_downloadApplets.m
+++ ios/Classes/Api/MOP_downloadApplets.m
@@ -11,6 +11,40 @@
 - (void)setupApiWithSuccess:(void (^)(NSDictionary<NSString *,id> * _Nonnull))success
                      failure:(void (^)(id _Nullable))failure
                       cancel:(void (^)(void))cancel {
+    if (![NSThread currentThread].isMainThread) {
+        [self _setupApiWithSuccess:success failure:failure cancel:cancel];
+        return;
+    }
+    // 在后台线程执行耗时操作
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        [self _setupApiWithSuccess:^(NSDictionary<NSString *,id> * _Nonnull data) {
+            // 成功回调回到主线程
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (success) {
+                    success(data);
+                }
+            });
+        } failure:^(id _Nullable error) {
+            // 失败回调回到主线程
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (failure) {
+                    failure(error);
+                }
+            });
+        } cancel:^{
+            // 取消回调回到主线程
+            dispatch_async(dispatch_get_main_queue(), ^{
+                if (cancel) {
+                    cancel();
+                }
+            });
+        }];
+    });
+}
+
+- (void)_setupApiWithSuccess:(void (^)(NSDictionary<NSString *,id> * _Nonnull))success
+                     failure:(void (^)(id _Nullable))failure
+                      cancel:(void (^)(void))cancel {
 
     if (!self.appIds || !self.apiServer) {
         failure(@"Missing required parameters");
@@ -50,5 +84,6 @@
         }
     }];
 }
+
 
 @end
